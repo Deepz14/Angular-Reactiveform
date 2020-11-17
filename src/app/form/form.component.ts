064353e952150f399c2bdd19e642f096d76112ab
iamdeepz <deepakchandrasekaran14@gmail.com>
2020-11-17 17:29:32 +0530

    modified validation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,6 +10,8 @@ export class FormComponent implements OnInit {
 
   userProfile : FormGroup;
 
+  submitted = false;
+
   validate = '';
 
   Qualifications : string[] = ['Elementary', 'Primary', 'Advanced']
@@ -25,34 +27,34 @@ export class FormComponent implements OnInit {
     this.userProfile = new FormGroup({
       user_name : new FormControl('', Validators.required),
       age : new FormControl('', Validators.required),
-      qualification : new FormControl(['']),
+      qualification : new FormControl([null]),
       gender : new FormControl('Male', Validators.required),
-      city : new FormControl(''),
-      state : new FormControl(['']),
-      zip : new FormControl(''),
-      agree : new FormControl('')
+      city : new FormControl('', Validators.required),
+      state : new FormControl([null]),
+      zip : new FormControl('', Validators.required),
+      agree : new FormControl('', Validators.required)
     })
   }
 
-  onSubmit(){
-    if(!this.userProfile.valid){
-      window.alert('Fields cannot be Empty...')
-    
-    }
-    else{
-      console.log(this.userProfile.value);
+  get f(){
+    return this.userProfile.controls;
+  }
 
+  onSubmit(){
+    this.submitted = true;
+
+    if(this.userProfile.invalid){
+      this.validate = 'is-invalid'
+      return;
     }
+
+    console.log(this.userProfile.value);
+
   }
 
   onClear(){
+    this.submitted = false;
     this.userProfile.reset()
-    if(!this.userProfile.valid){
-      this.validate = "is-invalid"
-    }
-    else{
-      this.validate = "is-valid"
-    }
   }
 
 }
