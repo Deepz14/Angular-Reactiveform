494ffd3d29d29d73b3c2cbf3e8254f3063efbd51
iamdeepz <deepakchandrasekaran14@gmail.com>
2020-11-17 15:08:12 +0530

    bootstrap-validation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,9 +10,13 @@ export class FormComponent implements OnInit {
 
   userProfile : FormGroup;
 
+  validate = '';
+
   Qualifications : string[] = ['Elementary', 'Primary', 'Advanced']
 
-   default = 'Select';
+  States : string[] = ['Usa','Canada','France']
+
+   
 
   constructor() { 
   }
@@ -22,13 +26,18 @@ export class FormComponent implements OnInit {
       user_name : new FormControl('', Validators.required),
       age : new FormControl('', Validators.required),
       qualification : new FormControl(['']),
-      gender : new FormControl('Male', Validators.required)
+      gender : new FormControl('Male', Validators.required),
+      city : new FormControl(''),
+      state : new FormControl(['']),
+      zip : new FormControl(''),
+      agree : new FormControl('')
     })
   }
 
   onSubmit(){
     if(!this.userProfile.valid){
       window.alert('Fields cannot be Empty...')
+    
     }
     else{
       console.log(this.userProfile.value);
@@ -38,6 +47,12 @@ export class FormComponent implements OnInit {
 
   onClear(){
     this.userProfile.reset()
+    if(!this.userProfile.valid){
+      this.validate = "is-invalid"
+    }
+    else{
+      this.validate = "is-valid"
+    }
   }
 
 }
